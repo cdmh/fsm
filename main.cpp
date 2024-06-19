f82b931b5d0c8520fc4806dd1861fcf0c0ee9d40
Craig Henderson <cdmh@users.noreply.github.com>
2024-06-19 17:46:30 +0100

    file refactoring

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-//#define TRACE_TOKENISER 1
+#define TRACE_TOKENISER 1
 #define TRACE_TOKENS    1
 #include "samples/pedestrian_crossing.hpp"
 #include "samples/tokeniser.hpp"
+#include "samples/cpp_tokeniser.hpp"
 
 int main()
 {
 //    pedestrian_crossing::run();
     tokeniser::run();
+    cpp_tokeniser::run();
 }
--- samples/tokeniser.hpp
+++ samples/tokeniser.hpp
@@ -900,170 +900,45 @@ class tokeniser_state_machine_generic
 };
 
 
-class tokeniser_state_machine : public tokeniser_state_machine_generic<tokeniser_state_machine>
+class state_machine : public tokeniser_state_machine_generic<state_machine>
 {
-};
-
-class cpp_tokeniser : public tokeniser_state_machine_generic<cpp_tokeniser>
-{
-  public:
-    enum class operator_type
-    {
-        assignment, asterisk,
-        binary_not, binary_not_eq, binary_or,
-        close_curly_bracket, close_paren, close_sq_bracket, colon, comma,
-        decrement, divide, divide_equal,
-        equal,
-        increment,
-        logical_and, logical_or,
-        minus, minus_equal, multiply_equal, // '*' is ambiguous, dependent on context
-        op_gt, op_gte, op_lt, op_lte, open_curly_bracket, open_paren, open_sq_bracket,
-        period, plus, plus_equal, power,
-        scope_resolution, semicolon, shift_left, shift_right,
-    };
-
-    using operator_info_type = std::pair<operator_type, std::string_view>;
-
-    cpp_tokeniser()
-    {
-#ifdef _DEBUG
-        for (auto op : operators_) {
-            for (auto ch : op.first)
-                assert(is_operator_char(ch));
-        }
-#endif
-    }
-
-    bool const greedy_operator_check(std::string_view op) const noexcept
-    {
-        return operators_.find(op) != operators_.cend();
-    }
-
-    operator_info_type operator_info(std::string_view op) const
-    {
-        auto it = operators_.find(op);
-        if (it == operators_.cend())
-            return {};
-        return it->second;
-    }
-
-    // use defaults for character types
-    using tokeniser_state_machine_generic<cpp_tokeniser>::is_oct_digit;
-    using tokeniser_state_machine_generic<cpp_tokeniser>::is_space;
-    using tokeniser_state_machine_generic<cpp_tokeniser>::is_numeric_digit;
-    using tokeniser_state_machine_generic<cpp_tokeniser>::is_operator_char;
-    using tokeniser_state_machine_generic<cpp_tokeniser>::is_quote_char;
-    using tokeniser_state_machine_generic<cpp_tokeniser>::is_token_separator;
-
-  private:
-    std::map<std::string_view, operator_info_type> operators_ = {
-        { "*",{ operator_type::asterisk, "asterisk" } },
-        { "&",{ operator_type::binary_or, "binary_or" } },
-        { "!",{ operator_type::binary_not, "binary_not" } },
-        { "!=", { operator_type::binary_not_eq, "binary_not_eq" } },
-        { "<",{ operator_type::op_lt, "op_lt" } },
-        { "<=", { operator_type::op_lte, "op_lte" } },
-        { ">",{ operator_type::op_gt, "op_gt" } },
-        { ">=", { operator_type::op_gte, "op_gte" } },
-        { "=",{ operator_type::assignment, "assignment" } },
-        { "==", { operator_type::equal, "equal" } },
-        { "+",{ operator_type::plus, "plus" } },
-        { "+=", { operator_type::plus_equal, "plus_equal" } },
-        { "-",{ operator_type::minus, "minus" } },
-        { "-=", { operator_type::minus_equal, "minus_equal" } },
-        { "/",{ operator_type::divide, "divide" } },
-        { "/=", { operator_type::divide_equal, "divide_equal" } },
-        { "*=", { operator_type::multiply_equal, "multiply_equal" } },
-        { ",",{ operator_type::comma, "comma" } },
-        { ".",{ operator_type::period, "period" } },
-        { "^",{ operator_type::power, "power" } },
-        { "(",{ operator_type::open_paren, "open_paren" } },
-        { ")",{ operator_type::close_paren, "close_paren" } },
-        { "[",{ operator_type::open_sq_bracket, "open_sq_bracket" } },
-        { "]",{ operator_type::close_sq_bracket, "close_sq_bracket" } },
-        { "{",{ operator_type::open_curly_bracket, "open_curly_bracket" } },
-        { "}",{ operator_type::close_curly_bracket, "close_curly_bracket" } },
-        { ":",{ operator_type::colon, "colon" } },
-        { ";",{ operator_type::semicolon, "semicolon" } },
-        { "&&", { operator_type::logical_and, "logical_and" } },
-        { "||", { operator_type::logical_or, "logical_or" } },
-        { "<<", { operator_type::shift_left, "shift_left" } },
-        { ">>", { operator_type::shift_right, "shift_right" } },
-        { "++", { operator_type::increment, "increment" } },
-        { "--", { operator_type::decrement, "decrement" } },
-        { "::", { operator_type::scope_resolution, "scope_resolution" } },
-    };
 };
 
 void run()
 {
-    {
-        tokeniser_state_machine tokeniser;
+    state_machine tokeniser;
 
-        std::vector<std::string> expressions = {
-            "0",
-            "0.3",
-            "4e3",
-            ".3e4",
-            "0e3",
-            ".1e-12",
-            ".1e+8",
-            "12.5",
-            "2.5",
-            ".5",
-            "5.",
-            "01",           // octal
-            "12345",
-            "678 ",
-            " 90",
-            " 123 456 ",
-            "1+2",
-            "0x38afe",      // hex
-            "0b101001",     // binary
-            "01723",        // octal
-            "123*0x2+ 0b10 / 19.234\t- 29^2",
-            // invalids
-            "1234.6789.2",
-            " 123x 45 678",
-            "++",
-        };
-
-        std::cout << "Generic tokeniser\n";
-        for (auto expr : expressions)
-            tokeniser.set_event(events::begin_parsing(expr));
-    }
-
-
-    cpp_tokeniser cpptokeniser;
-    std::vector<std::string> cpp_source = {
-        "next.empty()",
-        "operator<<",
-        "operator>>()",
-        "(*(++next))++",
+    std::vector<std::string> expressions = {
+        "0",
+        "0.3",
+        "4e3",
+        ".3e4",
+        "0e3",
+        ".1e-12",
+        ".1e+8",
+        "12.5",
+        "2.5",
+        ".5",
+        "5.",
+        "01",           // octal
+        "12345",
+        "678 ",
+        " 90",
+        " 123 456 ",
+        "1+2",
+        "0x38afe",      // hex
+        "0b101001",     // binary
+        "01723",        // octal
+        "123*0x2+ 0b10 / 19.234\t- 29^2",
+        // invalids
+        "1234.6789.2",
+        " 123x 45 678",
+        "++",
     };
-    std::cout << "\n\n\n\n\nC++ Tokeniser\n";
-    for (auto source : cpp_source)
-        cpptokeniser.set_event(events::begin_parsing(source));
 
-
-
-    bool quit = false;
-    while (!quit)
-    {
-        std::string expression;
-        std::getline(std::cin, expression);
-
-        if (expression == "q"  ||  expression == "Q")
-            quit = true;
-        else if (!expression.empty()) {
-            cpptokeniser.set_event(
-                events::begin_parsing(
-                    std::string_view(
-                        expression.cbegin(),
-                        expression.cend())));
-            cpptokeniser.async_wait_for_state<states::token_complete>([](){});
-        }
-    }
+    std::cout << "Generic tokeniser\n";
+    for (auto expr : expressions)
+        tokeniser.set_event(events::begin_parsing(expr));
 }
 
 }   // namespace tokeniser
